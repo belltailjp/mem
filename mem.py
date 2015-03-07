95a112993d308b4daf182531d0494d60e3f246e7
Daichi SUZUO <belltailjp@gmail.com>
2015-03-07 18:50:57 +0900

    Trap signals

--- mem.py
+++ mem.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import signal
 import time
 import argparse
 import subprocess
 
-def print_report(sub_command, status):
+def print_report(signalnum = None, stackframe = None):
     print("%(command)s  VmPeak: %(VmPeak)s  VmHWM: %(VmHWM)s" % {
             'command' : ' '.join(sub_command),
             'VmPeak' : make_readable(status['VmPeak']),
             'VmHWM' : make_readable(status['VmHWM'])
           })
+    exit(0)
 
 def make_readable(size_str):
     # size_str should be like "  1077992 kB"
@@ -21,17 +23,24 @@ def make_readable(size_str):
             ("%.1fGB" % (size / 1024**2)) if size < 1024**2 else\
             ("%.1fTB" % (size / 1024**3))
 
+proc = None
+sub_command = None
+status = None
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('sub_command', nargs='*')
     args = parser.parse_args()
+    sub_command = args.sub_command
+
+    signal.signal(signal.SIGINT, print_report)
+    signal.signal(signal.SIGTERM, print_report)
 
     proc = subprocess.Popen(args.sub_command)
     proc_path = "/proc/" + str(proc.pid) + "/status"
-    status = dict()
     while proc.poll() != 0:
         with open(proc_path, 'r') as f:
             status = dict([tuple(line.split(':')) for line in f.read().splitlines()])
         time.sleep(0.1)
 
-    print_report(args.sub_command, status)
+    print_report()
